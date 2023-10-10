7ebfc007718c920a13f57395adf79c25bf39518b
IsisTD <220126@fppiramide.com>
2023-10-10 13:05:54 +0200

    update

--- src/main/java/com/Horarios/horarios/Infraestructure/data/AulasRepositorySQL.java
+++ src/main/java/com/Horarios/horarios/Infraestructure/data/AulasRepositorySQL.java
@@ -1,4 +1,20 @@
 package com.Horarios.horarios.Infraestructure.data;
 
-public class AulasRepositorySQL {
+import com.Horarios.horarios.Domain.Aula;
+import com.Horarios.horarios.Domain.AulasRepository;
+import com.Horarios.horarios.Domain.Sesion;
+
+import java.util.List;
+
+public class AulasRepositorySQL implements AulasRepository {
+
+    @Override
+    public List<Aula> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Sesion> getSesionesFromAula(Aula aula) {
+        return null;
+    }
 }
