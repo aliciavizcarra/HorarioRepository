ccb41a480b15dd870b412b2867496fc3e95adcf3
IsisTD <220126@fppiramide.com>
2023-10-10 13:25:02 +0200

    update

--- src/main/java/com/Horarios/horarios/Infraestructure/data/AulasRepositorySQL.java
+++ src/main/java/com/Horarios/horarios/Infraestructure/data/AulasRepositorySQL.java
@@ -4,6 +4,7 @@ import com.Horarios.horarios.Domain.Aula;
 import com.Horarios.horarios.Domain.AulasRepository;
 import com.Horarios.horarios.Domain.Sesion;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class AulasRepositorySQL implements AulasRepository {
@@ -15,6 +16,18 @@ public class AulasRepositorySQL implements AulasRepository {
 
     @Override
     public List<Sesion> getSesionesFromAula(Aula aula) {
+        String consultaBusqueda="SELECT * FROM Sesiones JOIN Materias ON Sesiones.sesion = Materias.sesion";
+        ;
+        Connection connection=AulasConexionSQL.getCon();
+
+        try{
+
+        }
         return null;
     }
 }
+/*SELECT *
+FROM sesiones
+INNER JOIN seleccion ON sesiones.id_sesion = seleccion.id_sesion
+WHERE sesiones.id_sesion = 'valor_de_la_sesion';
+*/
