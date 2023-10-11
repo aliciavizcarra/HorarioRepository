590da5e15fed7ff62d35435f9b5bafdcf20b35c2
Alicia Vizcarra <220036@fppiramide.com>
2023-10-11 09:33:42 +0200

    sUBIDA

--- src/main/java/com/Horarios/horarios/Infraestructure/web/HorarioController.java
+++ src/main/java/com/Horarios/horarios/Infraestructure/web/HorarioController.java
@@ -1,9 +1,14 @@
 package com.Horarios.horarios.Infraestructure.web;
 
 import com.Horarios.horarios.Application.AulasCaseUses;
+import com.Horarios.horarios.Domain.Sesion;
 import com.Horarios.horarios.Infraestructure.data.AulasConexionSQL;
 import com.Horarios.horarios.Infraestructure.data.AulasRepositorySQL;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -14,4 +19,11 @@ public class HorarioController {
         this.aulasCaseUses=new AulasCaseUses(new AulasRepositorySQL());
     }
 
-}
+    @RequestMapping("/horario")
+        String horario(Model model){
+            List<Sesion> list = this.aulasCaseUses.getAll();
+            return "horario";
+        }
+    }
+
+
